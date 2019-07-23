914bb4e203ccb3305093c8681ff6416690f2f08e
Ihor <ShadoofJumper@gmail.com>
2019-07-23 22:04:08 +0300

    Initial commit

---
+++ Assets/Scripts/OrbitTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitTarget : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float rotSpeed = 1.5f; // rotate speed
+
+    private float _rotY; // variable for camera y position
+    private Vector3 _offset; // offset between camera and player 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y; // get camera euler y position
+        _offset = target.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        if (horInput != 0) // slow move using arrows buttons
+        {
+            _rotY += horInput * rotSpeed;
+        }
+        else
+        {
+            _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;// or faste move using mouse
+        }
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        // transform.position = target.position - (rotation * _offset); 
+        transform.position = target.position - (rotation * _offset) ; // поворачиваем положение камеры вокруг персонажа. 
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float rotSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+
+    private CharacterController _charController;
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero; // start in zero and then step by step add movement components
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+        if (horInput != 0 || verInput != 0) // if push buttons
+        {
+            movement.x = horInput * moveSpeed;
+            movement.z = verInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed); // ограничиваем движение по диагонали
+
+            Quaternion tmp = target.rotation; // save start orientation
+
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+            Quaternion diraction = Quaternion.LookRotation(movement); // savee direction
+            transform.rotation = Quaternion.Lerp(transform.rotation, diraction, rotSpeed * Time.deltaTime);// move from start to direction in speed rotSpeed 
+
+            movement *= Time.deltaTime;
+            _charController.Move(movement);
+        }
+    }
+}
